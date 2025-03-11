b5d3288fe602c3b7cd24697bf7fe06745c66f55e
Demba <dembouyz@hotmail.com>
2025-03-11 17:06:48 +0100

    Sauvegarde

---
+++ cartscript.js
@@ -0,0 +1,15 @@
+document.querySelector("#tickethack").addEventListener("click", function () {
+    document.location.href =
+      "http://127.0.0.1:5500/ticketHack/tickethack-frontend/index.html";
+  });
+  
+  document.querySelector("#p-cart").addEventListener("click", function () {
+    document.location.href =
+      "http://127.0.0.1:5500/ticketHack/tickethack-frontend/cart.html";
+  });
+  
+  document.querySelector("#p-booking").addEventListener("click", function () {
+    document.location.href =
+      "http://127.0.0.1:5500/ticketHack/tickethack-frontend/bookings.html";
+  });
+  
