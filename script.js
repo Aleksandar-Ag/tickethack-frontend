9013eb48d8586b35f3ed1e5740d96edad3933541
Aleksandar <gvozdenovic.aleksandre@gmail.com>
2025-03-11 17:36:32 +0100

    ajout event click sur search

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ document.querySelector("#p-booking").addEventListener("click", function () {
   document.location.href =
     "http://127.0.0.1:5500/ticketHack/tickethack-frontend/bookings.html";
 });
+
+document.querySelector('#search').addEventListener("click", () => {
+  fetch('http://localhost:3000/trips')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+})
